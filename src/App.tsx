86c0e3ff78b8adeddff2a4b0ea257665dff2deae
Boogle Staff <melvintseng@BoogleStaff.local>
2020-09-23 11:41:32 +0800

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { makeStyles, Grid, Button, TextField, Typography, InputAdornment, Checkbox, Link ,Box} from '@material-ui/core';
+import { AccountCircle, LockRounded, EmailRounded,Facebook } from '@material-ui/icons';
+import Boogle from './Boogle.png';
+import Bg from './bg.jpg';
+
+const useStyles = makeStyles({
+
+});
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid container style={{ minWidth: '100vh' }}>
+        <Grid item xs={12} sm={6}>
+          <img src={Bg} style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt="" />
+        </Grid>
+        <Grid container item xs={12} sm={6} alignItems="center" direction="column" justify="space-around" style={{ padding: 10 }}>
+          <div />
+          <div style={{ display: 'flex', flexDirection: "column" }}>
+            <Grid container justify="center">
+              <img src={Boogle} style={{ width: 130, marginTop: 120 }} alt="brand" />
+            </Grid>
+            <TextField margin="normal" label="Username or email" style={{ textAlign: "center" }} InputProps={{ startAdornment: <InputAdornment position="start"><AccountCircle /></InputAdornment> }}></TextField>
+            <TextField margin="normal" label="Password" type="password" style={{ textAlign: "center" }} InputProps={{ startAdornment: <InputAdornment position="start"><LockRounded /></InputAdornment> }}></TextField>
+
+          </div>
+          <div style={{marginBottom:400}}>
+          <Link
+            component="button"
+            variant="body2"
+            style={{  marginLeft: 70 , fontSize: 12}}
+            onClick={() => {
+              console.info("I'm a button.");
+
+            }}
+          >
+            Forgot your password?
+    </Link>
+    <div style={{display: 'flex',flexDirection: "column"}}>
+    <Button color="primary" variant="contained" style={{marginTop: 15 }}>Login</Button>
+    <Typography style={{ fontSize: 13, marginTop: 10, textAlign: "center" }}> Or sign in with</Typography>
+    </div>
+    <div style={{ display: 'flex', flexDirection: "row", padding: 5 }}>
+            <EmailRounded style={{marginLeft: 60}}></EmailRounded>
+            <Facebook style={{marginLeft: 10}}></Facebook>
+          </div>
+    </div>
+   
+         
+        </Grid>
+
+      </Grid>
     </div>
   );
 }
